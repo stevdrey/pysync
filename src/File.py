b84b88b66fe6d5e5abef3724fc8046fed3531590
stevdrey <stevdrey@gmail.com>
2013-01-05 22:53:30 -0600

    Se realizan algunas correcciones

--- src/File.py
+++ src/File.py
@@ -6,6 +6,7 @@ Created on 05/01/2013
 
 import shutil
 import os
+import sys
 
 class File(object):
     '''
@@ -19,40 +20,50 @@ class File(object):
         '''
     
     def copy(self, src, dest, opcion):
-        dst= os.path.join(dest, os.path.basename(src))
-        
-        # indica que el parametro: "src" es el paath de un archivo o un directorio
-        # que se desea copiar 
-        if opcion == 1:         
-            if os.path.exists(src):
-                if os.path.isfile(src):
-                    print 'Copiando el archivo {} al destino {} \n'.format(src, dst)
-                    
-                    shutil.copy2(src, dst)
-                
-                elif os.path.isdir(src):
-                    shutil.copytree(src, dst)
-            else:
-                print 'El archvio o directorio {}, no existe \n'.format(src)
-                
-                return
+        try:
+            # indica que el parametro: "src" es el paath de un archivo o un directorio
+            # que se desea copiar 
+            if opcion == 1:
+                print src
             
-        # indica que el parametro: "src" es el path de un arhivo el cual contiene
-        # los path de los archivos que se desean copiar  
-        elif opcion == 2:
-            with open(src, 'r') as path:
-                self.copy(path, dest, 1)
+                dst= os.path.join(dest, os.path.basename(src))
+                     
+                if os.path.exists(src):
+                    if os.path.isfile(src):
+                        print 'Copiando el archivo {} al destino {} \n'.format(src, dst)
+                    
+                        shutil.copy2(src, dst)
                 
+                    elif os.path.isdir(src):
+                        shutil.copytree(src, dst)
+                    else:
+                        print 'El archvio o directorio {}, no existe \n'.format(src)
+                
+                        sys.exit(2)
+            
+                # indica que el parametro: "src" es el path de un arhivo el cual contiene
+                # los path de los archivos que se desean copiar  
+                elif opcion == 2:
+                    with open(src, 'r') as f:
+                        for path in f:
+                            self.copy(str(path).replace('\n', ''), dest, 
+                                      1)
+                            print str(path).replace('\n', '')
+        except IOError as ex:
+            print ex
+            
+            sys.exit(2)
+            
         if opcion == 1 and os.path.isfile(src):
             print 'El archivo fue copiado satisfactoriamente \n'
             
         elif (opcion == 1 and os.path.isdir(src)) or opcion == 2:
-            print 'Los arhivos fueron copiados satisfactoriamente \n'
+            print 'Los archivos fueron copiados satisfactoriamente \n'
             
     def move(self, src, dest, opcion):
-        dst= os.path.join(dest, os.path.basename(src))
-        
         if opcion == 1:
+            dst= os.path.join(dest, os.path.basename(src))
+            
             if os.path.exists(src):
                 if os.path.isdir(src):
                     print 'Moviendo el directorio {} al destino'.format(src, dst)
--- src/Main.py
+++ src/Main.py
@@ -11,11 +11,11 @@ import argparse
 if __name__ == '__main__':
     parser= argparse.ArgumentParser()
     
-    parser.add_argument('-src', '--source', 
+    parser.add_argument('-src', '--source',
                         help='Indica la ruta del archvio o directorio fuente')
-    parser.add_argument('-dst', '--destination', 
+    parser.add_argument('-dst', '--destination', required=True,
                         help='Indica la ruta del directorio de destino')
-    parser.add_argument('-opt', '--option', type=int, 
+    parser.add_argument('-opt', '--option', type=int, required=True,
                         help='''
                         Indica la operacion que se desea realizar:
                         1. Copiar
@@ -26,23 +26,25 @@ if __name__ == '__main__':
     
     args= parser.parse_args()
     
-    if args != None:
+    print args
+    
+    if args:
         archivo= File()
         
-        if args['-lst'] == '':
+        if args.list_file == None and args.source != None:
             # indica que se desea copiar
-            if int(args['-opt']) == 1:
-                archivo.copy(args['-scr'], args['-dst'], 1)
+            if int(args.option) == 1:
+                archivo.copy(args.source, args.destination, 1)
                 
             # indica que se desea mover
-            elif int(args['-opt']) == 2:
-                archivo.move(args['-scr'], args['-dst'], 1)
+            elif int(args.option) == 2:
+                archivo.move(args.source, args.destination, 1)
                 
-        else:
+        elif args.list_file != None and args.source == None:
             # indica que se desea copiar
-            if int(args['-opt']) == 1:
-                archivo.copy(args['-lst'], args['-dst'], 1)
+            if int(args.option) == 1:
+                archivo.copy(args.list_file, args.destination, 2)
                 
             # indica que se desea mover
-            elif int(args['-opt']) == 2:
-                archivo.move(args['-lst'], args['-dst'], 1)
+            elif int(args.option) == 2:
+                archivo.move(args.list_file, args.destination, 2)
